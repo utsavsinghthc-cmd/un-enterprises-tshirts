0836cbd3c3160c5a49e6b76fcf57789bfee186a0
utsavsinghthc-cmd <utsavsinghthc@gmail.com>
2026-06-13 15:28:51 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Existing Firebase project configuration for UN Tees inquiries.
+// Replace these values with the Firebase config from the project settings before deployment.
+window.UN_TEES_FIREBASE_CONFIG = window.UN_TEES_FIREBASE_CONFIG || {
+  apiKey: "AIzaSyA0aCGaFQx_jEVaLJ5rDSPSfMWnPV_G9j0",
+  authDomain: "untees-258fe.firebaseapp.com",
+  projectId: "untees-258fe",
+  storageBucket: "untees-258fe.firebasestorage.app",
+  messagingSenderId: "803702984721",
+  appId: "1:803702984721:web:6c985c8296d8c9da9ce1b0",
+  measurementId: "G-YB31XMKL2H"
+};
